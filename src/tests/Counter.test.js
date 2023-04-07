f860b36bf18cfe8c40378fdb0c0d2fb5ea6f13ec
JRM128 <jmclaren@ufl.edu>
2023-04-07 19:51:04 -0400

    Update Counter.test.js
    Added all imports to allow unit tests to occur properly in the file. Also, added all necessary statements to have the unit tests run and test Counter.js correctly.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,33 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />)
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count');
+  expect(count).toHaveTextContent(0);
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count');
+  userEvent.click(screen.getByText('+'));
+  const count2 = screen.getByTestId('count');
+  expect(count.textContent).toEqual(count2.textContent);
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId('count');
+  userEvent.click(screen.getByText('-'));
+  const count2 = screen.getByTestId('count');
+  expect(count.textContent).toEqual(count2.textContent);
 });
